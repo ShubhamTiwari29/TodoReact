9424d904e1f404d6361e3e89d8dba7159af8c6e5
Shubham29lko@gmail.com <162909471+ShubhamTiwari29@users.noreply.github.com>
2024-05-04 00:42:55 +0530

    Dynamically Todo list generated

--- src/App.jsx
+++ src/App.jsx
@@ -9,27 +9,7 @@ function App() {
   return (
     <>
     <Navbar></Navbar>
-    <div className='container bg-violet-300 my-4 p-5 rounded-xl mx-auto min-h-screen'>
-      <div>
-        <div className="addtodo">
-          <h1 className='text-xl my-4 font-bold '>Add a todo</h1>
-          <input type="text" className='p-2 w-1/4 '  />
-          <button className='mx-2 bg-blue-400 hover:bg-blue-600 p-2 text-white rounded-md font-semibold'>Add</button>
-         
-        </div>
-        <h1 className='text-xl font-bold my-4'>Your Todos</h1>
-        <div className="todos bg-violet-400  ">
-          <div className="todo flex justify-between p-2">
-            <div className="text p-2">Buy Milk</div>
-
-<div className='Buttons'>
-<button className='mx-2 bg-blue-400 hover:bg-blue-600 p-2 text-white rounded-md font-semibold'>Comp</button>
-<button className='mx-2 bg-blue-400 hover:bg-blue-600 p-2 text-white rounded-md font-semibold'>Del</button>
-</div>
-          </div>
-        </div>
-      </div>
-    </div>
+    
      
     </>
   )
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,9 +1,56 @@
-import React from 'react'
+import React, { useState } from 'react'
+
 
 const Navbar = () => {
+
+const [todo, setTodo] = useState("")
+const [todos, setTodos] = useState([])
+
+const handleChange = (e)=>{
+  setTodo(e.target.value)
+  console.log(todos);
+}
+
+  const handleComplete = ()=>{
+
+  };
+
+  const handleDelete = ()=>{
+
+  };
+
+   const handleAdd = ()=>{
+setTodos([...todos, {todo, isCompleted: false}]);
+setTodo("");
+  };
+
   return (
     <div>
-      <h1>I am Navbar</h1>
+     <div className='container bg-violet-300 my-4 p-5 rounded-xl mx-auto min-h-screen'>
+      <div>
+        <div className="addtodo">
+          <h1 className='text-xl my-4 font-bold '>Add a todo</h1>
+          <input onChange={handleChange} value={todo} type="text" className='p-2 w-1/4 '  />
+          <button onClick={handleAdd} className='mx-2 bg-blue-400 hover:bg-blue-600 p-2 text-white rounded-md font-semibold'>Add</button>
+         
+        </div>
+        <h1 className='text-xl font-bold my-4'>Your Todos</h1>
+
+        <div className="todos bg-violet-400  ">
+         { todos.map((item , index)=>{
+          return<div key={index}  className="todo flex justify-between p-2 my-1 bg-blue-500">
+            <div  className="text p-2 ">{item.todo}</div>
+
+<div className='Buttons'>
+<button onClick={handleComplete} className='mx-2 bg-blue-400 hover:bg-blue-600 p-2 text-white rounded-md font-semibold'>Complete</button>
+<button onClick={handleDelete}  className='mx-2 bg-blue-400 hover:bg-blue-600 p-2 text-white rounded-md font-semibold'>Delete</button>
+</div>
+
+          </div>
+          }) }
+        </div>
+      </div>
+    </div>
     </div>
   )
 }
