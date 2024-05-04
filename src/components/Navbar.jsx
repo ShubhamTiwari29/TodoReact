3c1a0766e779e802341110940d88fa734a7a5ef0
Shubham29lko@gmail.com <162909471+ShubhamTiwari29@users.noreply.github.com>
2024-05-04 13:21:28 +0530

    complete and deleate feature added

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,26 +1,48 @@
 import React, { useState } from 'react'
+import { v4 as uuidv4 } from 'uuid';
+
 
 
 const Navbar = () => {
 
 const [todo, setTodo] = useState("")
 const [todos, setTodos] = useState([])
+const [checked, setChecked] = useState(false)
+
+ const handleEdit = () => {
+ 
+  };
 
 const handleChange = (e)=>{
   setTodo(e.target.value)
   console.log(todos);
-}
+} 
 
-  const handleComplete = ()=>{
+  const handleCheckbox = (e)=>{
+   let id = e.target.name;
+   console.log(id);
+   let index = todos.findIndex(item=>{
+    return item.id === id;
+   })
+   console.log(index);
+   let newTodos = [...todos];
+   newTodos[index].isCompleted = !newTodos[index].isCompleted;
+   setTodos(newTodos)
 
   };
 
-  const handleDelete = ()=>{
+  const handleDelete = (e, id)=>{
+    
+   let newTodos = todos.filter(item=>{
+    return item.id!==id
+   });
+   
+   setTodos(newTodos)
 
   };
 
    const handleAdd = ()=>{
-setTodos([...todos, {todo, isCompleted: false}]);
+setTodos([...todos, {id: uuidv4(), todo, isCompleted: false}]);
 setTodo("");
   };
 
@@ -37,17 +59,20 @@ setTodo("");
         <h1 className='text-xl font-bold my-4'>Your Todos</h1>
 
         <div className="todos bg-violet-400  ">
-         { todos.map((item , index)=>{
-          return<div key={index}  className="todo flex justify-between p-2 my-1 bg-blue-500">
-            <div  className="text p-2 ">{item.todo}</div>
-
-<div className='Buttons'>
-<button onClick={handleComplete} className='mx-2 bg-blue-400 hover:bg-blue-600 p-2 text-white rounded-md font-semibold'>Complete</button>
-<button onClick={handleDelete}  className='mx-2 bg-blue-400 hover:bg-blue-600 p-2 text-white rounded-md font-semibold'>Delete</button>
+          {todos.length === 0 && <div>No todos Here</div>}
+         { todos.map(item =>{
+          return<div key={item.id}  className="todo flex justify-between p-2 my-1 bg-blue-500">
+            <div className='flex'>
+            <input name={item.id} onChange={handleCheckbox} type="checkbox" value={item.isCompleted}   id="" />
+            <div  className={`text p-2 ${item.isCompleted ? "line-through " : "" }`}>{item.todo}</div>
 </div>
+          <div className='Buttons'>
+            <button onClick={handleEdit} className="mx-2 bg-blue-400 hover:bg-blue-600 p-2 text-white rounded-md font-semibold ">Edit </button>
+            <button onClick={(e)=>{handleDelete(e, item.id)}}  className='mx-2 bg-blue-400 hover:bg-blue-600 p-2 text-white rounded-md font-semibold'>Delete</button>
+          </div>
 
           </div>
-          }) }
+          })}
         </div>
       </div>
     </div>
